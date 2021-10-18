a7c277ec1cea76b12224496552fb7276030cc679
Jair Gómez Vásquez <36084661+Jairgo@users.noreply.github.com>
2021-10-18 11:18:25 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/dice_v2/Dice.java
@@ -0,0 +1,22 @@
+package com.example.dice_v2;
+
+import java.util.Random;
+
+public class Dice {
+    private final int max = 6;
+    private int actual;
+    private Random rand = new Random();
+
+    public Dice(){
+        this.actual = 1;
+    }
+
+    public Dice(int val){
+        this.actual = val;
+    }
+
+    public int girarDado(){
+        this.actual = rand.nextInt(6) + 1;
+        return this.actual;
+    }
+}
---
+++ app/src/main/java/com/example/dice_v2/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.dice_v2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Dice dado1, dado2;
+    private Button btnGirar;
+    private ImageView img1, img2;
+
+    private int images[] = {R.drawable._1,
+            R.drawable._2,
+            R.drawable._3,
+            R.drawable._4,
+            R.drawable._5,
+            R.drawable._6};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dado1 = new Dice();
+        dado2 = new Dice();
+
+        btnGirar = findViewById(R.id.btnGirar);
+        img1 = (ImageView) findViewById(R.id.img1);
+        img2 = (ImageView) findViewById(R.id.img2);
+
+        btnGirar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                img1.setImageResource(images[dado1.girarDado() - 1]);
+                img2.setImageResource(images[dado2.girarDado() - 1]);
+            }
+        });
+
+
+
+    }
+}
